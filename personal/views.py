38d7e5527a1dfd0d915077ba8f5d00f3f8008f0c
Rahul Saw <81628259+devrahuls@users.noreply.github.com>
2024-10-06 14:23:52 +0530

    Added Pagination

--- personal/views.py
+++ personal/views.py
@@ -2,7 +2,11 @@ from django.shortcuts import render
 from blog.models import BlogPost
 from operator import attrgetter
 from blog.views import get_blog_queryset
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
+
+
+BLOG_POSTS_PER_PAGE = 1
 # Create your views here.
 def home_screen_view(request):
     print(request.headers)
@@ -27,11 +31,23 @@ def home_screen_view(request):
 
     query = ""
     if request.GET:
-        query = request.GET['q'] #Get the query from the search bar.
+        query = request.GET.get('q', '') #Get the query from the search bar. if it is empty then pass the empty '' to the url.
         context['query'] = str(query) #Display the query in the search bar.
 
     #Get all the blog post, and then sort it by the date_updated field in the descending order(reverse=True).
     blog_posts = sorted(get_blog_queryset(query), key=attrgetter('date_updated'), reverse=True)
     context['blog_posts'] = blog_posts
 
+    #Pagination
+    page = request.GET.get('page', 1) #Get the page number from the url, if it is not there then set it to 1.
+    blog_posts_paginator = Paginator(blog_posts, BLOG_POSTS_PER_PAGE) #Show 5 blog posts per page.
+
+    try:
+        blog_posts = blog_posts_paginator.page(page) #Get the blog posts for that page.
+    except PageNotAnInteger:
+        blog_posts = blog_posts_paginator.page(BLOG_POSTS_PER_PAGE) #If the page is not an integer, then show the first page.
+    except EmptyPage:
+        blog_posts = blog_posts_paginator.page(blog_posts_paginator.num_pages) #If the page is empty, then show the last page.
+    
+    context['blog_posts'] = blog_posts
     return render(request, 'personal/home.html', context)
