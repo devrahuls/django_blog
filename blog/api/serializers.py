2bf08131bcecc424abdeaa439efacf83025f2e41
Rahul Saw <81628259+devrahuls@users.noreply.github.com>
2024-10-24 21:02:20 +0530

    Return the username of the blog while GET the blog object.

--- blog/api/serializers.py
+++ blog/api/serializers.py
@@ -3,6 +3,15 @@ from rest_framework import serializers
 from blog.models import BlogPost
 
 class BlogPostSerializer(serializers.ModelSerializer):
+
+    #A field, that holds the username of the blog.
+    username = serializers.SerializerMethodField('get_username_from_author')
+
     class Meta:
         model = BlogPost
-        fields = ['title', 'body', 'image', 'date_updated']
+        fields = ['title', 'body', 'image', 'date_updated', 'username'] #The reason I dont put author here from BlogPost model, bcz it will return a number.
+    
+    #A method, responsible for returning the username to the SerializerMethodField, from the blog_post.author
+    def get_username_from_author(self, blog_post):
+        username = blog_post.author.username
+        return username
